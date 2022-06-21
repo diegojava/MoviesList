7dc60802954aa982de715d79116e10efd49e5ddf
Diego Armando Jaimes Vázquez. CCORP <diego_jaimes@consucorp.com.mx>
2022-06-21 10:02:50 -0500

    added functionality to get more api daily consume

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
+const api = () => {
+  let apis = ['k_irla67zt', 'k_r45g9qpi']
+  return Math.floor(Math.random() * apis.length)
+}
+
 export const environment = {
   production: true,
-  apiUrl: 'https://imdb-api.com/en/API/MostPopularMovies/k_irla67zt'
+  apiUrl: 'https://imdb-api.com/en/API/MostPopularMovies/' + api
 };
